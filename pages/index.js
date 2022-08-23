7e3ffa5b083eedcb581668af411cc86f60af1af3
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-23 11:54:04 -0700

    Fixed Save Resume

--- pages/index.js
+++ pages/index.js
@@ -134,7 +134,7 @@ export default function Home({ posts }) {
               <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
                   <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r  from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <a href='/public/resume.pdf' download>
+                  <a href="https://github.com/andykr1k/resume/raw/main/AK_Resume.pdf">
                     <span className="relative flex items-center divide-x divide-gray-600 rounded-lg bg-white px-7 py-4 leading-none dark:bg-black">
                       <span className="flex items-center space-x-5">
                         <svg
