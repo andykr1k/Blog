ca0672317b95b9feac81d8c290c3a8b2d0e19b41
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-08-22 20:08:23 -0700

    Finishing Touches

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,13 +1,39 @@
 const projectsData = [
   {
-    title: 'EdvisorWeb',
+    title: 'Edvisor',
     description: `Artificial Intelligence made for students transfering to a four year college out of community college.`,
-    imgSrc: '',
-    href: '',
+    href: 'https://edvisor.netlify.app/',
     github: 'https://github.com/andykr1k/EdvisorWeb',
     tech1: 'React',
     tech2: 'Firebase',
     tech3: 'Chakra UI',
+  },
+  {
+    title: 'Jada Ganim',
+    description: `Personal Multi-Page Portfolio.`,
+    href: 'https://jadaganim.com',
+    github: 'https://github.com/andykr1k/Website_JadaPortfolio',
+    tech1: 'React',
+    tech2: 'Tailwind',
+    tech3: 'Chakra UI',
+  },
+  {
+    title: 'React Linktree',
+    description: `Personal Multi-Page Linktree.`,
+    href: 'https://fluffy-frangollo-433461.netlify.app/',
+    github: 'https://github.com/andykr1k/ReactLinktree',
+    tech1: 'React',
+    tech2: 'Firebase',
+    tech3: 'Chakra UI',
+  },
+  {
+    title: 'Andrew Krikorian',
+    description: `Personal Multi-Page Portfolio and Blog.`,
+    href: 'https://rebrand.ly/andykr1k/',
+    github: 'https://github.com/andykr1k/blog',
+    tech1: 'Next.js',
+    tech2: 'Tailwind',
+    tech3: 'Radix UI',
   },
 ]
 
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,7 +6,7 @@ const siteMetadata = {
   snippets: 'Reuseable code snippets collected by Andrew',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://musing.vercel.app',
+  siteUrl: 'https://blog-andykr1k.vercel.app/blog',
   siteRepo: 'https://github.com/andykr1k/blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
