2406a5f10dd18d663c3ca0ad4889dd83bb6ee60c
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-09-04 16:09:37 -0700

    Fixing Logos

--- pages/_document.js
+++ pages/_document.js
@@ -10,47 +10,47 @@ class MyDocument extends Document {
           <link
             rel="apple-touch-icon"
             sizes="76x76"
-            href="/static/favicons/apple-touch-icon-76x76.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="apple-touch-icon-precomposed"
             sizes="57x57"
-            href="/static/favicons/apple-touch-icon-57x57.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="apple-touch-icon-precomposed"
             sizes="114x114"
-            href="/static/favicons/apple-touch-icon-114x114.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="apple-touch-icon-precomposed"
             sizes="72x72"
-            href="/static/favicons/apple-touch-icon-72x72.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="apple-touch-icon-precomposed"
             sizes="144x144"
-            href="/static/favicons/apple-touch-icon-144x144.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="apple-touch-icon-precomposed"
             sizes="60x60"
-            href="/static/favicons/apple-touch-icon-60x60.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="apple-touch-icon-precomposed"
             sizes="120x120"
-            href="/static/favicons/apple-touch-icon-120x120.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="apple-touch-icon-precomposed"
             sizes="76x76"
-            href="/static/favicons/apple-touch-icon-76x76.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="apple-touch-icon-precomposed"
             sizes="152x152"
-            href="/static/favicons/apple-touch-icon-152x152.png"
+            href="/static/favicons/apple-touch-icon.png"
           />
           <link
             rel="icon"
