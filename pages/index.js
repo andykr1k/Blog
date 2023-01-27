cb8f808cb4995bf9cedd9bd41ec35a0504044ce7
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-01-27 10:46:45 -0800

    Fixed Resume Download Button

--- pages/index.js
+++ pages/index.js
@@ -134,7 +134,7 @@ export default function Home({ posts }) {
               <div className="my-2 grid items-start gap-8">
                 <div className="group relative">
                   <div className="animate-tilt absolute -inset-0.5 rounded-lg bg-gradient-to-r  from-pink-600 to-purple-600 opacity-50 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                  <a href="./Resume.pdf"
+                  <a href="./static/Resume.pdf"
                       alt="Resume"
                       target="_blank"
                       rel="noopener noreferrer"
